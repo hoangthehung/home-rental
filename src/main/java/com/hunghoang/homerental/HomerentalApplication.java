e08a41ad594cfac05c14d03f564ba8abb486b527
Mike Hung <mike.hung@singtel.com>
2024-03-26 17:04:11 +0800

    initialize project

---
+++ src/main/java/com/hunghoang/homerental/HomerentalApplication.java
@@ -0,0 +1,13 @@
+package com.hunghoang.homerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomerentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomerentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hunghoang/homerental/HomerentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunghoang.homerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomerentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
